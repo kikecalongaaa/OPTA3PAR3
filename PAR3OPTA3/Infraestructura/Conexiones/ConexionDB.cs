574b8df78902a32be81817163cc939819e57b2e4
kikecalongaaa <57482596+kikecalongaaa@users.noreply.github.com>
2023-11-23 22:11:38 -0300

    OPTA3PAR3_TOKKEN
    Parcial3 Codigo vs c#  y bd en sqlserver 2022

---
+++ PAR3OPTA3/Infraestructura/Conexiones/ConexionDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Npgsql;
+namespace Infraestructura.Conexiones
+{
+	//USO BD SQLSERVER CON POSTMAN  , CONEXION DIRECTA EN HERRAMIENTAS
+
+
+	//public class ConexionDB
+	//{
+	//	private string cadenaConexion;
+	//	private NpgsqlConnection conexion;
+
+	//	public ConexionDB(string cadenaConexion)
+	//	{
+	//		this.cadenaConexion = cadenaConexion;
+	//		conexion = new NpgsqlConnection(cadenaConexion);
+	//	}
+
+	//	public NpgsqlConnection GetConexion()
+	//	{
+
+	//		if(conexion.State != System.Data.ConnectionState.Open)
+	//			conexion.Open();
+	//		return conexion;
+	//	}
+	//}
+}
+
---
+++ PAR3OPTA3/Infraestructura/Datos/UsuariosDatos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using Infraestructura.Conexiones;
+using Infraestructura.Modelos;
+
+namespace Infraestructura.Datos
+{
+    public class UsuarioDatos
+    {
+        //CRUD USUARIOS  
+
+        public static void insertarusuario(UsuarioModel usuario)
+        {
+          
+            var comando = new Npgsql.NpgsqlCommand("INSERT INTO usuario(idusuario, IDPersona, nombre_usuario, contraseña, Nivel, estado)" +
+                                                "VALUES(@idusuario, @IdPersona, @nombre_usuario, @contraseña, @nivel, @estado)");
+            comando.Parameters.AddWithValue("IDPersona", usuario.IDPersona);
+            comando.Parameters.AddWithValue("nombre_usuario", usuario.nombre_usuario);
+            comando.Parameters.AddWithValue("estado", usuario.estado);
+            comando.Parameters.AddWithValue("idusuario", usuario.idusuario);
+            comando.Parameters.AddWithValue("contraseña", usuario.estado);
+            comando.Parameters.AddWithValue("nivel", usuario.idusuario);
+            comando.ExecuteNonQuery();
+        }
+
+        public UsuarioModel obtenerusuarioPorId(int id)
+        {
+            
+            var comando = new Npgsql.NpgsqlCommand($"Select * from usuario where idusuario = {id}");
+            using var reader = comando.ExecuteReader();
+            if (reader.Read())
+            {
+                return new UsuarioModel
+                {
+                    idusuario = reader.GetInt32("idusuario"),
+                    contraseña = reader.GetString("contraseña"),
+                    nombre_usuario = reader.GetString("nombre_usuario"),
+                    nivel = reader.GetString("nivel"),
+                    IDPersona = reader.GetString("idPersona"),
+                    estado = reader.GetString("estado")
+                };
+            }
+            return null;
+        }
+
+        public void Modificarusuario(UsuarioModel usuario)
+        {
+            
+            var comando = new Npgsql.NpgsqlCommand($"UPDATE usuario SET contraseña = '{usuario.contraseña}', " +
+                                                          $"nombre_usuario = '{usuario.nombre_usuario}', " +
+                                                          $"estado = '{usuario.estado}' " +
+                                                          $"nivel = '{usuario.nivel}' " +
+                                                          $"IdPersona = '{usuario.IDPersona}' " +
+                                                $" WHERE idusuario = {usuario.idusuario}");
+
+            comando.ExecuteNonQuery();
+        }
+    }
+}
